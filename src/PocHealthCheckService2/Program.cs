56b02d920689f64894b2ea2b071f29ec30c45bf9
Bruno Bennotti <brunobennotti@gmail.com>
2022-12-09 16:04:28 -0300

    feat: verificação serviço externo feat: implementação custom check feat: implementação custom css ui

--- src/PocHealthCheckService2/Program.cs
+++ src/PocHealthCheckService2/Program.cs
@@ -1,22 +1,15 @@
 using HealthChecks.UI.Client;
 using Microsoft.AspNetCore.Diagnostics.HealthChecks;
-using PocHealthCheckWithUi;
+using PocHealthCheckService2;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Add services to the container.
+
 builder.Services.AddCors();
 builder.Services.AddControllers();
-// Add services to the container.
 builder.Services.AddHealthChecks()
     .AddCheck<CustomHealthChecks>("Health Checks customizavel");
-builder.Services.AddHealthChecksUI(options =>
-{
-    options.SetEvaluationTimeInSeconds(5);
-    options.MaximumHistoryEntriesPerEndpoint(10);
-    options.AddHealthCheckEndpoint("PocHealthCheckWithUI", "/health");
-    options.AddHealthCheckEndpoint("PocHealthCheckService2", "https://localhost:7213/health");
-})
-.AddInMemoryStorage();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -26,11 +19,10 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseDeveloperExceptionPage();
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-app.UseCors(x => x.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod());
+
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
@@ -41,11 +33,6 @@ app.UseHealthChecks("/health", new HealthCheckOptions
 {
     Predicate = p => true,
     ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
-});
-
-app.UseHealthChecksUI(options => {
-    options.UIPath = "/dashboard";
-    options.AddCustomStylesheet("custom.css");
 });
 
 app.Run();
---
+++ src/PocHealthCheckWithUi/CustomHealthChecks.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+namespace PocHealthCheckWithUi
+{
+    public class CustomHealthChecks : IHealthCheck
+    {
+        public Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = default)
+        {
+            var random = new Random();
+            var list = new List<HealthStatus> { HealthStatus.Healthy, HealthStatus.Unhealthy, HealthStatus.Degraded };
+            int index = random.Next(list.Count);
+
+            return Task.FromResult(new HealthCheckResult(
+                status: list[index],
+                description: list[index] == HealthStatus.Healthy ? "Tudo certo" : list[index] == HealthStatus.Unhealthy ? "Algo errado não esta certo." : "Deu ruim"
+            ));
+        }
+    }
+}
